8dfe3c2ff21a5a58bc3fe485e545df63b890d7ff
曹弘扬 <296786475@qq.com>
2019-01-04 10:52:33 +0800

    Initial Commit

---
+++ Adonis_20190104_OpenGL_Environment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adonis_20190104_OpenGL_Environment
+//
+//  Created by Adonis_HongYang on 2019/1/4.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Adonis_20190104_OpenGL_Environment/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Adonis_20190104_OpenGL_Environment
+//
+//  Created by Adonis_HongYang on 2019/1/4.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
