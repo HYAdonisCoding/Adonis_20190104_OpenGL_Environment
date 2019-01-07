5413eb2e9fd324aa1ce60edaa88d0854eb415849
曹弘扬 <296786475@qq.com>
2019-01-07 11:02:49 +0800

    绘制一个圆形

--- Adonis_20190104_OpenGL_Environment/Classes/main.cpp
+++ Adonis_20190104_OpenGL_Environment/Classes/main.cpp
@@ -6,87 +6,49 @@
 //  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
 //
 
-#include "GLShaderManager.h"
+#include <iostream>
 
-#include "GLTools.h"
+#include "math3d.h"
 
 #include <GLUT/GLUT.h>
 
-GLBatch triangleBatch;
-
-GLShaderManager shaderManager;
-
-//窗口大小改变时接受新的宽度和高度，其中0,0代表窗口中视口的左下角坐标，w，h代表像素
-
-void ChangeSize(int w,int h)
-
+void draw()
 {
+    //1.设置清屏颜色
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    //2.执行清屏操作
+    //GL_COLOR_BUFFER_BIT
     
-    glViewport(0,0, w, h);
+    glClear(GL_COLOR_BUFFER_BIT);
     
-}
-
-//为程序作一次性的设置
-
-void SetupRC()
-
-{
+    //3.设置绘图颜色
+    glColor3f(1.0f, 0.0f, 0.0f);
     
-    //设置背影颜色
+    //设置绘图的坐标系统
+    //左\右\上\下\远\近
+    //glOrtho(0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 1.0f);
     
-    glClearColor(0.0f,0.0f,1.0f,1.0f);
+    //开始渲染
+    //GL_LINES形成n/2条线条
+    //GL_POINTS把每个顶点作为一个点去处理,形成N个点
+    //GL_LINE_STRIP 从第一个顶点到最后一个顶点连接成一组线段。n-1线段
+    //GL_LINE_LOOP 从第一个顶点到最后一个顶点依次相连。n线程
+    //
+    glBegin(GL_POLYGON);
     
-    //初始化着色管理器
+    //设置顶点
+    const int n = 55;
+    const GLfloat R = 0.5f;
+    const GLfloat pi = 3.1415926f;
     
-    shaderManager.InitializeStockShaders();
+    for (int i = 0; i < n; i++) {
+        glVertex2f(R * cos(2 * pi / n * i), R * sin(2 * pi / n * i));
+    }
     
-    //设置三角形，其中数组vVert包含所有3个顶点的x,y,笛卡尔坐标对。
+    //结束渲染
+    glEnd();
     
-    GLfloat vVerts[] = {
-        
-        -0.5f,0.0f,0.0f,
-        
-        0.5f,0.0f,0.0f,
-        
-        0.0f,0.5f,0.0f,
-        
-    };
-    
-    //批次处理
-    
-    triangleBatch.Begin(GL_TRIANGLES,3);
-    
-    triangleBatch.CopyVertexData3f(vVerts);
-    
-    triangleBatch.End();
-    
-}
-
-//开始渲染
-
-void RenderScene(void)
-
-{
-    
-    //清除一个或一组特定的缓冲区
-    
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    
-    //设置一组浮点数来表示红色
-    
-    GLfloat vRed[] = {1.0f,0.0f,0.0f,1.0f};
-    
-    //传递到存储着色器，即GLT_SHADER_IDENTITY着色器，这个着色器只是使用指定颜色以默认笛卡尔坐标第在屏幕上渲染几何图形
-    
-    shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vRed);
-    
-    //提交着色器
-    
-    triangleBatch.Draw();
-    
-    //将在后台缓冲区进行渲染，然后在结束时交换到前台
-    
-    glutSwapBuffers();
+    glFlush();
     
 }
 
@@ -94,51 +56,29 @@ int main(int argc,char* argv[])
 
 {
     
-    //设置当前工作目录，针对MAC OS X
-    
-    gltSetWorkingDirectory(argv[0]);
     
     //初始化GLUT库
     
-    glutInit(&argc, argv);
+    glutInit(&argc, (char **)argv);
     
     /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
      
      双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
     
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
+    //glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
     
     //GLUT窗口大小，标题窗口
     
-    glutInitWindowSize(800,600);
+//    glutInitWindowSize(800,600);
     
-    glutCreateWindow("Triangle");
+    glutCreateWindow("Adonis_Window");
     
-    //注册回调函数
-    
-    glutReshapeFunc(ChangeSize);
-    
-    glutDisplayFunc(RenderScene);
-    
-    //驱动程序的初始化中没有出现任何问题。
-    
-    GLenum err = glewInit();
-    
-    if(GLEW_OK != err) {
-        
-        fprintf(stderr,"glew error:%s\n",glewGetErrorString(err));
-        
-        return 1;
-        
-    }
-    
-    //调用SetupRC
-    
-    SetupRC();
+    //绘制函数
+    glutDisplayFunc(draw);
     
     glutMainLoop();
     
-    return 0;
+    return 1;
     
 }
 
